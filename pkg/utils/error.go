4d565eb17d55ad3184522c37b152cb3e0f218a41
Michel Calheiros <calheiros97@gmail.com>
2020-12-13 23:31:06 -0300

    fix: added validation error for single fields

--- pkg/utils/error.go
+++ pkg/utils/error.go
@@ -6,42 +6,53 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-// ValidationError is the base structure for validation errors
+// ValidationError is the base structure for validation errors, contains user-friendly error data
 type ValidationError struct {
 	Message       string
 	Specification string `json:",omitempty"`
 	Field         string
 }
 
-// ParseValidationErrors will return a slice containing user-friendly ValidationError structs
+func validateFieldError(err validator.FieldError) ValidationError {
+	var errorMessage string
+	var errorSpecification string
+
+	// empty value means we should say that the field is required
+	// while a non-empty value means the information is invalid
+	if err.Value() == "" {
+		errorMessage = fmt.Sprintf("%s is %s", err.Field(), err.Tag())
+	} else {
+		errorMessage = fmt.Sprintf("%s is invalid", err.Field())
+	}
+
+	if err.ActualTag() == "min" {
+		errorSpecification = fmt.Sprintf("%s should be at least %s characters", err.Field(), err.Param())
+	}
+
+	if err.ActualTag() == "max" {
+		errorSpecification = fmt.Sprintf("%s should be at most %s characters", err.Field(), err.Param())
+	}
+
+	validationError := ValidationError{
+		Message:       errorMessage,
+		Specification: errorSpecification,
+		Field:         err.Field(),
+	}
+
+	return validationError
+}
+
+// ParseValidationErrors will return a slice containing ValidationError structs
 func ParseValidationErrors(errors validator.ValidationErrors) (validationErrors []ValidationError) {
 	for _, err := range errors {
-		var errorMessage string
-		var errorSpecification string
-
-		// empty value means we should say that the field is required
-		// while a non-empty value means the information is invalid
-		if err.Value() == "" {
-			errorMessage = fmt.Sprintf("%s is %s", err.Field(), err.Tag())
-		} else {
-			errorMessage = fmt.Sprintf("%s is invalid", err.Field())
-		}
-
-		if err.ActualTag() == "min" {
-			errorSpecification = fmt.Sprintf("%s should be at least %s characters", err.Field(), err.Param())
-		}
-
-		if err.ActualTag() == "max" {
-			errorSpecification = fmt.Sprintf("%s should be at most %s characters", err.Field(), err.Param())
-		}
-
-		validationError := ValidationError{
-			Message:       errorMessage,
-			Specification: errorSpecification,
-			Field:         err.Field(),
-		}
+		validationError := validateFieldError(err)
 
 		validationErrors = append(validationErrors, validationError)
 	}
 	return validationErrors
+}
+
+// ParseValidationError will return a single ValidationError instance
+func ParseValidationError(err validator.FieldError) ValidationError {
+	return validateFieldError(err)
 }
